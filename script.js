a007f7b4de45c8ece825525ccdd3d18fbeec73f8
Nijel <nmanalo543@gmail.com>
2022-03-12 22:12:32 +0000

    started making functions for adding books, still deciding on how to style books

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("js working");
+const library = document.querySelector(".library");
+
+let books = [];
+
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBook(name, author, pages, read) {
+  books.push(new Book(name, author, pages, read));
+}
+
+function displayBooks() {
+  books.forEach(addToScreen);
+  
+  function addToScreen(book) {
+    //
+  }
+}
