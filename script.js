975569d40c106a9c850791b26851c7da4d09fc44
Nijel <nmanalo543@gmail.com>
2022-03-14 07:52:00 +0000

    wrote function for displaying books on the page, added basic book styles

--- script.js
+++ script.js
@@ -13,10 +13,27 @@ function addBook(name, author, pages, read) {
   books.push(new Book(name, author, pages, read));
 }
 
-function displayBooks() {
-  books.forEach(addToScreen);
-  
-  function addToScreen(book) {
-    //
-  }
+function displayCurrentBooks() {
+  books.forEach(function (book) {
+    const newBook = document.createElement("li");
+    newBook.classList.add("book");
+
+    const name = document.createElement("h3");
+    name.textContent = book.name;
+
+    const author = document.createElement("h4");
+    author.textContent = "by " + book.author;
+
+    const pages = document.createElement("p");
+    pages.textContent = book.pages + " pages";
+
+    const read = document.createElement("div");
+    read.textContent = book.read ? "finished" : "reading...";
+
+    newBook.append(name, author, pages, read);
+    library.append(newBook);
+  });
 }
+
+addBook("Funny Book", "Hilarious Guy", 69, true);
+displayCurrentBooks();
