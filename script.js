1b0cc850b091b4c26404d0a2a13f3b28b6907dfb
Nijel <nmanalo543@gmail.com>
2022-03-17 22:08:20 +0000

    pretty much done with styling

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const addBookButton = document.querySelector(".add-book");
 
 let books = [];
 
-function Book(name, author, pages, read) {
+function Book(name, author, pages, finished) {
   this.name = name;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.finished = finished;
 }
 
-function addBook(name, author, pages, read) {
-  books.push(new Book(name, author, pages, read));
+function addBook(name, author, pages, finished) {
+  books.push(new Book(name, author, pages, finished));
 }
 
 // removes all books from the screen
@@ -36,8 +36,19 @@ function displayCurrentBooks() {
     const newBook = document.createElement("li");
     newBook.classList.add("book");
 
-    const name = document.createElement("h3");
-    name.textContent = book.name;
+    // this top row thing is for styling
+    const top = document.createElement("div");
+    top.classList.add("book-top");
+
+      const name = document.createElement("h3");
+      name.textContent = book.name;
+
+      const closeButton = document.createElement("div");
+      closeButton.classList.add("remove-book");
+      closeButton.textContent = "x";
+
+    top.append(name, closeButton);
+    // end of top row
 
     const author = document.createElement("h4");
     author.textContent = "by " + book.author;
@@ -45,14 +56,22 @@ function displayCurrentBooks() {
     const pages = document.createElement("p");
     pages.textContent = book.pages + " pages";
 
-    const read = document.createElement("div");
-    read.textContent = book.read ? "finished" : "reading...";
+    const finished = document.createElement("div");
+    finished.classList.add("finished");
+    finished.textContent = book.finished ? "finished" : "reading...";
 
-    newBook.append(name, author, pages, read);
+    newBook.append(top, author, pages, finished);
     library.insertBefore(newBook, library.lastElementChild);
   });
 }
 
 addBook("Funny Book", "Hilarious Guy", 69, true);
+addBook("Trash Book", "me", 400, false);
+addBook("Trash Book", "me", 400, false);
+addBook("Trash Book", "me", 400, false);
+addBook("Trash Book", "me", 400, false);
+addBook("Trash Book", "me", 400, false);
+addBook("Trash Book", "me", 400, false);
+addBook("Trash Book", "me", 400, false);
 addBook("Trash Book", "me", 400, false);
 displayCurrentBooks();
