4a3328d62eb0b9f9d7f4ddd9ca583fc30d2e97f7
Nijel Manalo <nmanalo543@gmail.com>
2022-03-20 17:06:15 +0000

    you can now make the form appear and disappear, you can now add books with the + button, still working on removing books

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const library = document.querySelector(".library");
 const addBookButton = document.querySelector(".add-book");
 
+// form elements
+const addBookForm = document.querySelector("form");
+const closeFormButton = document.querySelector(".close-form");
+const nameField = document.querySelector("#name");
+const authorField = document.querySelector("#author");
+const pagesField = document.querySelector("#pages");
+const finishedField = document.querySelector("#finished");
+const submitButton = document.querySelector(".submit");
+
 let books = [];
 
 function Book(name, author, pages, finished) {
@@ -10,6 +19,24 @@ function Book(name, author, pages, finished) {
   this.finished = finished;
 }
 
+// for opening the 'add book' form
+addBookButton.addEventListener("click", function (ev) {
+  addBookForm.classList.remove("hidden");
+  nameField.focus();
+});
+
+// for closing the form
+closeFormButton.addEventListener("click", closeForm);
+window.addEventListener("keydown", function (ev) {
+  if(ev.keyCode == 27)
+    closeForm();
+});
+// the actual function
+function closeForm() {
+  addBookForm.classList.add("hidden");
+}
+
+// adds a book to the array
 function addBook(name, author, pages, finished) {
   books.push(new Book(name, author, pages, finished));
 }
@@ -33,8 +60,6 @@ function displayCurrentBooks() {
 
   // add current books back to the page
   books.forEach(function (book) {
-    const newBook = document.createElement("li");
-    newBook.classList.add("book");
 
     // this top row thing is for styling
     const top = document.createElement("div");
@@ -60,10 +85,24 @@ function displayCurrentBooks() {
     finished.classList.add("finished");
     finished.textContent = book.finished ? "finished" : "reading...";
 
+    const newBook = document.createElement("li");
+    newBook.classList.add("book");
+    newBook.dataset.index = 0;
     newBook.append(top, author, pages, finished);
     library.insertBefore(newBook, library.lastElementChild);
   });
 }
+
+// submitting the form
+submitButton.addEventListener("click", function (ev) {
+  const name = nameField.value;
+  const author = authorField.value;
+  const pages = pagesField.value;
+  const finished = finishedField.checked;
+
+  addBook(name, author, pages, finished);
+  displayCurrentBooks();
+});
 
 addBook("Funny Book", "Hilarious Guy", 69, true);
 addBook("Trash Book", "me", 400, false);
